25ff1374a9eaa7dcc2eeae54f4c267ac8b308d1e
Luisca <luisca1985@gmail.com>
2019-01-03 23:53:56 -0500

    Configuración del puerto. Instalacion de nodemon. Instalacion de morgan y compression.

--- sitio/backend/config/express.js
+++ sitio/backend/config/express.js
@@ -4,11 +4,21 @@
 // Cargar las dependencias del módulo
 const config = require('./config');
 const express = require('express');
+const morgan = require('morgan');
+const compress = require('compression');
 
 // Definir el mérodo de configuración de Express
 module.exports = function(){
     // Crear una nueva instancia de la aplicación Express
     const app = express();
+
+    // Usar la variable "NODE_ENV" para activar los middleware "morgan" logger o 'compress'
+	if(process.env.NODE_ENV ==='development'){
+		app.use(morgan('dev'));
+	} else if(process.env.NODE_ENV === 'production'){
+		app.use(compress());
+	}
+
     // Devolver la instancia de la aplicación Express
     return app;
 }
--- sitio/backend/index.js
+++ sitio/backend/index.js
@@ -10,10 +10,13 @@ const express = require('./config/express');
 // Crear una nueva instancia aplicación Express
 var app = express();
 
+// Configuración del puerto
+app.set('port', process.env.PORT || 3000);
+
 // Usar la instancia de la aplicación Express para que escuche en el puerto '3000'
-app.listen(3000, () => {
+app.listen(app.get('port'), () => {
     // Hacer Log del status del server a la consola
-	console.log('Servidor ejecutándose en http://localhost:3000/');
+	console.log('Servidor ejecutándose en el puerto',app.get('port'));
 });
 
 // Usar la prpiedad module.exports para exponer nuestra nuestra instancia de la aplicación Express para uso externo
